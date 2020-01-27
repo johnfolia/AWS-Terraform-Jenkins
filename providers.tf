a980044cd4d1e1dc3da494fa20881a38351cbdf0
johnfolia <johnfolia@yahoo.com>
2020-01-27 13:02:10 -0800

    updated backend

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "${var.region}"
 }
+terraform {
+  backend "s3" {
+    bucket = "kams-devops2"
+    key = "terraform.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "terraform-lock"
+    encrypt = true
+  }
+}
