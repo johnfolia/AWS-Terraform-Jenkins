33a81599dab7462b52b5f547faa06913236b202f
johnfolia <johnfolia@yahoo.com>
2020-01-27 12:55:20 -0800

    updated Ansible config

--- providers.tf
+++ providers.tf
@@ -1,12 +1,3 @@
 provider "aws" {
   region = "${var.region}"
 }
-terraform {
-  backend "s3" {
-    bucket = "kams-devops"
-    key = "terraform.tfstate"
-    region = "us-west-2"
-    dynamodb_table = "terraform-lock"
-    encrypt = true
-  }
-}
